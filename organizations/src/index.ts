e61a9c496beb0cebe83940ca2a344b64e431a08e
b-haytham <boussarsarhaytham@gmail.com>
2020-12-18 13:05:39 +0100

    [organizations]: connect to mongo database

--- organizations/src/index.ts
+++ organizations/src/index.ts
@@ -1,6 +1,30 @@
 import app from './app';
+import mongoose from 'mongoose';
 
 const start = async () => {
+	if (!process.env.JWT_SECRET) {
+		throw new Error('JWT_SECRET must be defined');
+	}
+
+	if (!process.env.COOKIE_SECRET) {
+		throw new Error('COOKIE_SECRET must be defined');
+	}
+
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI must be defined');
+	}
+
+	try {
+		await mongoose.connect(`${process.env.MONGO_URI}/organizations`, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('>>>> Connected to DB');
+	} catch (error) {
+		console.error(error);
+	}
+
 	app.listen(3000, () => console.log('>>>> Listening on Post 3000'));
 };
 
