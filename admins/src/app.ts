04661327a3e411f6f71c13c2ca16fdb2642a94ba
b-haytham <boussarsarhaytham@gmail.com>
2021-01-24 14:31:12 +0100

    [admins]: current-user route

--- admins/src/app.ts
+++ admins/src/app.ts
@@ -5,6 +5,7 @@ import { expressErrorHandler, NotFoundError } from '@dabra/survey_common';
 import createAdminRouter from './routes/createAdmin'
 import loginRouter from './routes/login'
 import logoutRouter from './routes/logout'
+import currentAdmin from './routes/currentAdmin'
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.get('/api/admins', (req, res) => res.send('hello [admins]'));
 app.use(createAdminRouter)
 app.use(loginRouter)
 app.use(logoutRouter)
+app.use(currentAdmin)
 
 
 app.all('*', () => {
