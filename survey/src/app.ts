5fe05d1b126e116f5e5fb132be73fee37e9818e8
b-haytham <boussarsarhaytham@gmail.com>
2021-01-24 22:34:39 +0100

    [survey]: connect to mongo database

--- survey/src/app.ts
+++ survey/src/app.ts
@@ -1,7 +1,29 @@
 import express from 'express';
+import cookieSession from 'cookie-session'
+import { expressErrorHandler, NotFoundError } from '@dabra/survey_common';
 
 const app = express();
 
+app.set('trust proxy', true)
+
+app.use(express.json())
+
+app.use(cookieSession({
+	signed: false,
+	secure: process.env.NODE_ENV !== 'test',
+	secret: process.env.COOKIE_SECRET,
+}))
+
 app.get('/', (req, res) => res.send('hello [survey_schema]'));
+
+
+app.all('*', () => {
+	console.log("Not Found");
+	throw new NotFoundError();
+});
+
+app.use(expressErrorHandler);
+
+
 
 export default app;
--- survey/src/index.ts
+++ survey/src/index.ts
@@ -1,6 +1,34 @@
+import mongoose from 'mongoose'
+
 import app from './app';
 
+
 const start = async () => {
+
+
+	if (!process.env.JWT_SECRET) {
+		throw new Error('JWT_SECRET must be defined');
+	}
+
+	if (!process.env.COOKIE_SECRET) {
+		throw new Error('COOKIE_SECRET must be defined');
+	}
+
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI must be defined');
+	}
+
+	try {
+		await mongoose.connect(`${process.env.MONGO_URI}/survey`, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('>>>> Connected to DB');
+	} catch (error) {
+		console.error(error);
+	}
+
 	app.listen(3000, () =>
 		console.log('>>>> [survey_schema]:  Listening on Post 3000')
 	);
